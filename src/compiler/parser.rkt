d593fd5933838a440fd69accd6fc5502ee158884
xhs <chef@dark.kitchen>
2015-06-17 20:37:00 +0800

    wee-lang parser

--- src/compiler/parser.rkt
+++ src/compiler/parser.rkt
@@ -3,7 +3,7 @@
 (require parser-tools/lex)
 (require parser-tools/lex-sre)
 (require ragg/support)
-(require "parser.rkt")
+(require "grammer.rkt")
 
 (define (string->boolean s)
   (match s
@@ -12,12 +12,14 @@
 
 (define tokenize
   (lexer
+   ((: "//" (* (- any-char #\newline)) #\newline)
+    (token 'COMMENT lexeme #:skip? #t))
+   ; boolean
    ((or "true" "false")
     (token 'BOOLEAN (string->boolean lexeme)))
-   ("->"
-    (token 'ARROW lexeme))
-   ("recur"
-    (token 'RECUR lexeme))
+   ; keyword
+   ((or "->" "=" ":=" "end")
+    lexeme)
    ; identifier
    ((: (or alphabetic #\$ #\_)
        (* (or alphabetic numeric #\$ #\_)))
@@ -28,17 +30,33 @@
     (token 'INTEGER (string->number lexeme)))
    ; primitive
    ((or #\+ #\- #\* #\/)
-    (token 'PRIMITIVE lexeme))
-   (#\=
-    (token 'ASSIGN lexeme))
-   (#\(
-    (token 'LEFT-PAREN lexeme))
-   (#\)
-    (token 'RIGHT-PAREN lexeme))
+    lexeme)
+   ; delimeter
+   ((or #\( #\))
+    lexeme)
+   ; skip
    ((+ (or blank #\return #\newline))
     (token 'WHITESPACE lexeme #:skip? #t))
    ((eof)
     (void))))
 
-(define in (open-input-string "add = (a b) -> a + b"))
-(tokenize in)
+(define in (open-input-string "
+c = 0
+
+count = () ->
+  // c is free variable
+  c := c + 1
+  c
+end
+
+count()
+"))
+
+(define (scan port)
+  (let loop ((t (tokenize port))
+             (acc '()))
+    (if (void? t)
+        (reverse acc)
+        (loop (tokenize port) (cons t acc)))))
+
+(syntax->datum (parse (scan in)))
